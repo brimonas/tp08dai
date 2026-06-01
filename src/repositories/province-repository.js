1f972a9fd88a2f699a82ad1c8f9cd38a8ce5d3d1
Android Studio <devandroid@ort>
2026-06-01 10:15:11 -0300

    repository

--- src/repositories/province-repository.js
+++ src/repositories/province-repository.js
@@ -1,77 +1,137 @@
 import Db from '../configs/db-config.js';
+import pkg from 'pg'
+const {Client, Pool} = pkg
 
 export default class ProvinceRepository {
-    constructor() {
-        this.db = new Db();
-    }
+   
 
     getAllAsync = async () => {
-        return await this.db.queryAll('SELECT * FROM provincias');
-    }
-
-
+        let returnArray = null;
+        const client = new Client(Db);
+        try{
+            await client.connect();
+            const sql = 'SELECT * FROM provincias';
+            const result= await client.query(sql);
+            await client.end();
+            returnArray = result.rows;
+        }
+        catch(error){
+            console.log(error)
+        }
+            return returnArray
+        }
+       
     getByIdAsync = async (id) => {
+        let returnEntity = null;
+        const client = new Client(Db);
 
-        console.log(`ProvinceRepository.getByIdAsync(${id})`)
+        try {
+            await client.connect();
 
-        const sql = `SELECT * FROM provincias WHERE id = $1`
+            const sql = 'SELECT * FROM provincias WHERE id = $1';
+            const result = await client.query(sql, [id]);
 
-        return await this.db.queryOne(sql, [id])
+            await client.end();
+
+            returnEntity = result.rows[0];
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+        return returnEntity;
     }
 
-    createAsync = async (entity) => {
+     createAsync = async (entity) => {
+        let rowCount = 0;
+        const client = new Client(Db);
 
-        console.log(`ProvinceRepository.createAsync(${JSON.stringify(entity)})`)
+        try {
+            await client.connect();
 
-        const sql = `
-            INSERT INTO provincias
-            (name, full_name, latitude, longitude, display_order)
- /*cambiar tabla prov*/
-            VALUES ($1, $2, $3, $4, $5)
-        `
+            const sql = `
+                INSERT INTO provincias
+                (name, full_name, latitude, longitude, display_order)
+                VALUES ($1, $2, $3, $4, $5)
+            `;
 
-        const values = [
-            entity.name,
-            entity.full_name,
-            entity.latitude,
-            entity.longitude,
-            entity.display_order
-        ]
+            const values = [
+                entity.name,
+                entity.full_name,
+                entity.latitude,
+                entity.longitude,
+                entity.display_order
+            ];
 
-        return await this.db.queryRowCount(sql, values)
+            const result = await client.query(sql, values);
+
+            await client.end();
+
+            rowCount = result.rowCount;
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+        return rowCount;
     }
-    updateAsync = async (entity) => {
+     updateAsync = async (entity) => {
+        let rowCount = 0;
+        const client = new Client(Db);
 
-        console.log(`ProvinceRepository.updateAsync(${JSON.stringify(entity)})`);
+        try {
+            await client.connect();
 
-        const sql = `
-            UPDATE provincias
-            SET
-                name = $2,
-                full_name = $3,
-                latitude = $4,
-                longitude = $5,
-                display_order = $6
-            WHERE id = $1
-        `;
+            const sql = `
+                UPDATE provincias
+                SET
+                    name = $2,
+                    full_name = $3,
+                    latitude = $4,
+                    longitude = $5,
+                    display_order = $6
+                WHERE id = $1
+            `;
 
-        const values = [
-            entity.id,
-            entity.name,
-            entity.full_name,
-            entity.latitude,
-            entity.longitude,
-            entity.display_order
-        ];
+            const values = [
+                entity.id,
+                entity.name,
+                entity.full_name,
+                entity.latitude,
+                entity.longitude,
+                entity.display_order
+            ];
 
-        return await this.db.queryRowCount(sql, values);
+            const result = await client.query(sql, values);
+
+            await client.end();
+
+            rowCount = result.rowCount;
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+        return rowCount;
     }
-     deleteByIdAsync = async (id) => {
+      deleteByIdAsync = async (id) => {
+        let rowCount = 0;
+        const client = new Client(Db);
 
-        console.log(`ProvinceRepository.deleteByIdAsync(${id})`);
+        try {
+            await client.connect();
 
-        const sql = `DELETE FROM provincias WHERE id = $1`;
+            const sql = 'DELETE FROM provincias WHERE id = $1';
+            const result = await client.query(sql, [id]);
 
-        return await this.db.queryRowCount(sql, [id]);
+            await client.end();
+
+            rowCount = result.rowCount;
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+        return rowCount;
     }
 }
--- src/services/province-service.js
+++ src/services/province-service.js
@@ -2,37 +2,38 @@ import ProvinceRepository from "../repositories/province-repository.js"
 
 export default class ProvinceService {
 
-    constructor() {
-
-        this.repository = new ProvinceRepository()
-    }
-
+   
     getAllAsync = async () => {
-       const returnArray = await this.repository.getAllAsync();
+         const repo = new ProvinceRepository()
+       const returnArray = await repo.getAllAsync();
         return returnArray;
     }
 
     getByIdAsync = async (id) => {
-        const returnEntity = await this.repository.getByIdAsync(id);
+         const repo = new ProvinceRepository()
+           console.log(`ProvinceRepository.getByIdAsync(${id})`)
+        const returnEntity = await repo .getByIdAsync(id);
         return returnEntity;
     }
 
     createAsync = async (entity) => {
-
-        console.log(`CursosService.getByIdAsync(${id})`);
-        const returnEntity = await this.repository.getByIdAsync(id);
+             const repo = new ProvinceRepository()
+        console.log(`ProvinceRepository.getByIdAsync(${id})`);
+        const returnEntity = await repo.getByIdAsync(id);
         return returnEntity;
     }
 
     updateAsync = async (entity) => {
-
-        const rowsAffected = await this.repository.updateAsync(entity);
+         const repo = new ProvinceRepository()
+         console.log(` ProvinceRepository.updateAsync(${JSON.stringify(entity)})` );
+        const rowsAffected = await repo.updateAsync(entity);
         return rowsAffected;
     }
 
     deleteByIdAsync = async (id) => {
-
-        return await this.repository.deleteByIdAsync(id); const rowsAffected = await this.CursosRepository.deleteByIdAsync(id);
+         const repo = new ProvinceRepository()
+            console.log(`ProvinceRepository.deleteByIdAsync(${id})` );
+        return await repo.deleteByIdAsync(id); const rowsAffected = await this.CursosRepository.deleteByIdAsync(id);
         return rowsAffected;
     }
 }
